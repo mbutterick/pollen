c4d55e793862fbdb4e7efecd3074aec5aa7ca0ee
Matthew Butterick <mb@mbtype.com>
2014-02-24 00:05:36 -0800

    add html->xexpr

--- tools.rkt
+++ tools.rkt
@@ -1,5 +1,10 @@
 #lang racket/base
 (require racket/list "debug.rkt" "predicates.rkt")
-(provide (all-from-out  racket/list "debug.rkt" "predicates.rkt"))
+(provide (all-defined-out)
+         (all-from-out  racket/list "debug.rkt" "predicates.rkt"))
 
 
+(require xml racket/string)
+
+(define (html->xexpr . xs)
+  (string->xexpr (string-join xs "")))
