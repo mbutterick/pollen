8fdaaf7c7b3eee654fa1ef7718ec4e3ee1f83df5
Matthew Butterick <mb@mbtype.com>
2014-02-27 14:47:50 -0800

    nit

--- top.rkt
+++ top.rkt
@@ -3,7 +3,7 @@
 ;; Changes the default behavior of #%top.
 ;; Unbound identifiers are allowed, and treated as the 
 ;; tag in a txexpr (with the rest of the expression treated as the body)
-;; To suppress this behavior, use bound/c to wrap any name.
+;; To suppress this behavior, use def/c to wrap any name.
 ;; If that name isn't already defined, you'll get the usual syntax error.
 
 (require (for-syntax racket/base))
