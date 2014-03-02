8cf461392a13457f162f51a6af55f8b222ce5d5b
Matthew Butterick <mb@mbtype.com>
2014-03-01 20:56:50 -0800

    add markdown dependency

--- info.rkt
+++ info.rkt
@@ -1,5 +1,5 @@
 #lang info
 (define collection "pollen")
 (define scribblings '(("scribblings/pollen.scrbl" ())))
-(define deps '("txexpr" "sugar"))
+(define deps '("txexpr" "sugar" "markdown"))
 (define raco-commands '(("pollen" pollen/command "issue Pollen command" #f)))
