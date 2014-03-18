097f874015d9956b3219f4c8f9d80a7b195b7fb3
Matthew Butterick <mb@mbtype.com>
2014-03-17 20:52:18 -0700

    edit

--- main.rkt
+++ main.rkt
@@ -1,7 +1,7 @@
 #lang racket/base
 (require pollen/main-base)
-(define+provide-module-begin-in-mode world:mode-preproc)
+(define+provide-module-begin-in-mode world:mode-preproc) ; because default mode in submodule is preproc
 
 (module reader racket/base
   (require pollen/reader-base)
-  (define+provide-reader-in-mode world:mode-auto))
+  (define+provide-reader-in-mode world:mode-auto)) ; because default mode in file is auto
