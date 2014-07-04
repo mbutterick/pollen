a618993b001b203cfc74248cdbe6b1e2c8fa9410
Matthew Butterick <mb@mbtype.com>
2014-07-04 11:48:56 -0700

    declare dependency on 'base' rather than 'racket'

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "pollen")
-(define deps '(("racket" #:version "6.0") "txexpr" "sugar" "markdown"))
+(define deps '(("base" #:version "6.0") "txexpr" "sugar" "markdown"))
 (define scribblings '(("scribblings/pollen.scrbl" (multi-page))))
 (define raco-commands '(("pollen" pollen/raco "issue Pollen command" #f)))
 (define compile-omit-paths '("tests" "raco.rkt"))
