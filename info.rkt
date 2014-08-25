a834a99861b82581d188f1f0c51ffe63f14ace1a
Matthew Butterick <mb@mbtype.com>
2014-08-24 17:40:08 -0700

    add 2htdp dependency

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "pollen")
-(define deps '(("base" #:version "6.0") "txexpr" "sugar" ("markdown" #:version "0.18")))
+(define deps '(("base" #:version "6.0") "txexpr" "sugar" ("markdown" #:version "0.18") "2htdp"))
 (define scribblings '(("scribblings/pollen.scrbl" (multi-page))))
 (define raco-commands '(("pollen" pollen/raco "issue Pollen command" #f)))
 (define compile-omit-paths '("tests" "raco.rkt"))
