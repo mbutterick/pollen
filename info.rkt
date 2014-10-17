99eb35a5333e987365fa214909720da020592905
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2014-10-17 11:47:52 -0400

    Fix dependencies.

--- info.rkt
+++ info.rkt
@@ -1,6 +1,8 @@
 #lang info
 (define collection "pollen")
-(define deps '(("base" #:version "6.0") "txexpr" "sugar" ("markdown" #:version "0.18") "htdp"))
+(define deps '(("base" #:version "6.0") "txexpr" "sugar" ("markdown" #:version "0.18") "htdp"
+               "at-exp-lib" "html-lib" "rackjure" "web-server-lib"))
+(define build-deps '("plot-gui-lib" "rackunit-lib" "scribble-lib"))
 (define update-implies '("txexpr" "sugar"))
 (define scribblings '(("scribblings/pollen.scrbl" (multi-page))))
 (define raco-commands '(("pollen" pollen/raco "issue Pollen command" #f)))
