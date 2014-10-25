a22931f359a81ee2e20060d569e83f9aa6b6d841
Matthew Butterick <mb@mbtype.com>
2014-10-24 19:12:15 -0700

    update dependencies

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "pollen")
 (define deps '(("base" #:version "6.0") "txexpr" "sugar" ("markdown" #:version "0.18") "htdp"
-               "at-exp-lib" "html-lib" "rackjure" "web-server-lib"))
+               "at-exp-lib" "html-lib" "rackjure" "web-server-lib" "scribble-text-lib"))
 (define build-deps '("plot-gui-lib" "rackunit-lib" "scribble-lib"))
 (define update-implies '("txexpr" "sugar"))
 (define scribblings '(("scribblings/pollen.scrbl" (multi-page))))
