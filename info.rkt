7298d2d0c386b53db3b7b049bd5f5c95060a958f
Matthew Butterick <mb@mbtype.com>
2015-04-11 09:15:27 -0700

    Revert "omit thorny test temporarily"
    This reverts commit 67da2f9fc4f456d1505451a1b393725bf1358069.

--- info.rkt
+++ info.rkt
@@ -7,4 +7,3 @@
 (define scribblings '(("scribblings/pollen.scrbl" (multi-page))))
 (define raco-commands '(("pollen" pollen/raco "issue Pollen command" #f)))
 (define compile-omit-paths '("tests" "raco.rkt"))
-(define test-omit-paths '("tests/test-langs.rkt"))
