4834b8e1d62dfe26bd587cc7b9353b44f98c347e
Matthew Butterick <mb@mbtype.com>
2015-05-17 12:15:29 -0700

    update build dependencies

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 (define collection "pollen")
 (define deps '("base" "txexpr" "sugar" ("markdown" #:version "0.18") "htdp"
                "at-exp-lib" "html-lib" "rackjure" "web-server-lib" "scribble-text-lib"))
-(define build-deps '("plot-gui-lib" "rackunit-lib" "scribble-lib" "racket-doc"))
+(define build-deps '("plot-gui-lib" "rackunit-lib" "scribble-lib" "racket-doc" "plot-doc" "scribble-doc" "slideshow-doc" "web-server-doc"))
 (define update-implies '("txexpr" "sugar"))
 (define scribblings '(("scribblings/pollen.scrbl" (multi-page))))
 (define raco-commands '(("pollen" (submod pollen/raco main) "issue Pollen command" #f)))
