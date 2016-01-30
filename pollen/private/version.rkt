b0275f1526f0208282164d76ceb63aa12f83a598
Matthew Butterick <mb@mbtype.com>
2016-01-30 15:06:05 -0800

    version number adjustments

--- pollen/private/version.rkt
+++ pollen/private/version.rkt
@@ -14,8 +14,12 @@
 
 (define (get-version)
   (define major-version 0)
-  (define minor-version (convert (~r ts #:base 26)))
-  (format "~a.~a" major-version minor-version))
+  (define minor-version 9)
+  (define pollen-birthday 1375340400) ; 8/1/2013, 12:00:00 AM GMT-7:00 DST
+  (define seconds-per-year (* 60 60 24))
+  (define build-time (- (file->value ts-file) pollen-birthday))
+  (define-values (build-days build-seconds) (quotient/remainder build-time seconds-per-year))
+  (format "~a.~a.~a.~a" major-version minor-version build-days build-seconds))
 
 
 (define pollen:version (get-version))
