abe8db6aa595783507476b3f05b25d988f9e75b6
Matthew Butterick <mb@mbtype.com>
2016-01-30 12:27:53 -0800

    update-info module

--- pollen/info.rkt
+++ pollen/info.rkt
@@ -1,5 +1,5 @@
 #lang info
-(define version "1.0")
+(define version "1.eskfage")
 (define deps '("base" "txexpr" "sugar" ("markdown" #:version "0.18") "htdp"
                "at-exp-lib" "html-lib" "rackjure" "web-server-lib" "scribble-text-lib" "rackunit-lib"
                "gui-lib"))
---
+++ pollen/private/update-info.rkt
@@ -0,0 +1,11 @@
+#lang racket/base
+(require racket/runtime-path racket/file pollen/private/version)
+
+(define-runtime-path info-file "../info.rkt")
+
+(define str (file->string info-file))
+
+(define newstr
+  (regexp-replace #rx"\\(define version .*?\\)" str (format "(define version ~v)" (pollen:version))))
+
+(display-to-file newstr info-file #:exists 'replace)
