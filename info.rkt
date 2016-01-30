ced29f92c860ff3af6b281dbea4ace4a6ec849d4
Matthew Butterick <mb@mbtype.com>
2016-01-30 12:14:31 -0800

    move info

--- info.rkt
+++ info.rkt
@@ -1,8 +1,2 @@
 #lang info
-(define version "1.0")
 (define collection 'multi)
-(define deps '("base" "txexpr" "sugar" ("markdown" #:version "0.18") "htdp"
-               "at-exp-lib" "html-lib" "rackjure" "web-server-lib" "scribble-text-lib" "rackunit-lib"
-               "gui-lib"))
-(define build-deps '("plot-gui-lib" "scribble-lib" "racket-doc" "rackunit-doc" "plot-doc" "scribble-doc" "slideshow-doc" "web-server-doc"))
-(define update-implies '("txexpr" "sugar"))
--- pollen/info.rkt
+++ pollen/info.rkt
@@ -1,4 +1,11 @@
 #lang info
+(define version "1.0")
+(define deps '("base" "txexpr" "sugar" ("markdown" #:version "0.18") "htdp"
+               "at-exp-lib" "html-lib" "rackjure" "web-server-lib" "scribble-text-lib" "rackunit-lib"
+               "gui-lib"))
+(define build-deps '("plot-gui-lib" "scribble-lib" "racket-doc" "rackunit-doc" "plot-doc" "scribble-doc" "slideshow-doc" "web-server-doc"))
+(define update-implies '("txexpr" "sugar"))
+
 (define scribblings '(("scribblings/pollen.scrbl" (multi-page))))
 (define raco-commands '(("pollen" (submod pollen/private/command raco) "issue Pollen command" #f)))
 (define compile-omit-paths '("test" "tools" "server-extras" "scribblings/third-tutorial-files"))
