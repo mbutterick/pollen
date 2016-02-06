8b58aad2023f30caf2d5bc07f1c7f9f1ce40de0b
Leif Andersen <leif@leifandersen.net>
2016-02-06 10:06:04 -0500

    Apparently DrRacket is needed as a dep.

--- info.rkt
+++ info.rkt
@@ -5,5 +5,6 @@
 (define deps '("base" "txexpr" "sugar" ("markdown" #:version "0.18") "htdp"
                "at-exp-lib" "html-lib" "rackjure" "web-server-lib" "scribble-text-lib" "rackunit-lib"
                "gui-lib"))
-(define build-deps '("plot-gui-lib" "scribble-lib" "racket-doc" "rackunit-doc" "plot-doc" "scribble-doc" "slideshow-doc" "web-server-doc"))
+(define build-deps '("plot-gui-lib" "scribble-lib" "racket-doc" "rackunit-doc"
+                     "plot-doc" "scribble-doc" "slideshow-doc" "web-server-doc" "drracket"))
 (define update-implies '("txexpr" "sugar"))
