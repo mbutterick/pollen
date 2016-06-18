f120c377211337e45cfb059f068761ed3e1fc264
Matthew Butterick <mb@mbtype.com>
2016-06-18 14:01:21 -0700

    add missing dependency

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 
 (define version "1.1")
 (define deps '("base" ["txexpr" #:version "0.2"] ["sugar" #:version "0.2"] ["markdown" #:version "0.18"] "htdp"
-               "at-exp-lib" "html-lib" "rackjure" "web-server-lib" "scribble-text-lib" "rackunit-lib"
+               "at-exp-lib" "html-lib" "rackjure" "web-server-lib" "scribble-lib" "scribble-text-lib" "rackunit-lib"
                "gui-lib"))
 (define build-deps '("plot-gui-lib" "scribble-lib" "racket-doc" "rackunit-doc"
                      "plot-doc" "scribble-doc" "slideshow-doc" "web-server-doc" "drracket"))
