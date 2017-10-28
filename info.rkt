3b6de79fdd6bb7b8b4d150a4d42cf0685b232d4e
Leif Andersen <leif@leifandersen.net>
2017-10-28 13:08:03 -0400

    Add string-constants-lib to deps list.

--- info.rkt
+++ info.rkt
@@ -4,7 +4,7 @@
 (define version "1.2")
 (define deps '("base" ["txexpr" #:version "0.2"] ["sugar" #:version "0.2"] ["markdown" #:version "0.18"] "htdp"
                "at-exp-lib" "html-lib" "rackjure" "web-server-lib" "scribble-lib" "scribble-text-lib" "rackunit-lib"
-               "gui-lib"))
+               "gui-lib" "string-constants-lib"))
 (define build-deps '("plot-gui-lib" "scribble-lib" "racket-doc" "rackunit-doc"
                      "plot-doc" "scribble-doc" "slideshow-doc" "web-server-doc" "drracket"))
 (define update-implies '("txexpr" "sugar"))
