11c50310e67d12b8672b53a6905c8479e366d85c
Matthew Butterick <mb@mbtype.com>
2017-11-20 23:19:42 -0800

    comedy of error

--- pollen/mode.rkt
+++ pollen/mode.rkt
@@ -91,7 +91,7 @@ Intractable problem; unavoidable limitation.
                (fallback)))
          (case key
            [(color-lexer)
-            (define lexer-maker (dynamic-require 'syntax-color/scribble-lexer 'make-scribble-lexer #f))
+            (define lexer-maker (dynamic-require 'syntax-color/scribble-lexer 'make-scribble-lexer (λ () #f)))
             (if lexer-maker
                 (lexer-maker #:command-char #\◊)
                 (fallback))]
