d5a8ffabcc89e8a0982a7e58f8ae156c26fa3f17
Matthew Butterick <mb@mbtype.com>
2018-02-19 14:12:25 -0800

    test `current-metas`

--- pollen/test/pollen.rkt
+++ pollen/test/pollen.rkt
@@ -1,4 +1,5 @@
 #lang racket/base
+(require racket/string pollen/core)
 
 ;; This will prevent tests from littering their directory
 ;; with pollen-cache files.
@@ -6,3 +7,9 @@
   (provide (all-defined-out))
   (define compile-cache-active #f)
   (define render-cache-active #f))
+
+(provide test-current-metas)
+(define (test-current-metas)
+  (if (current-metas)
+      (string-join (sort (map symbol->string (hash-keys (current-metas))) string<?) " ")
+      "false"))
