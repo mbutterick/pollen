f558b641e7a31a0a8fe8cdda703e01a50a4f9932
Matthew Butterick <mb@mbtype.com>
2018-10-30 12:00:59 -0700

    logging the right way

---
+++ pollen/private/log.rkt
@@ -0,0 +1,14 @@
+#lang racket/base
+(require racket/format
+         racket/string
+         "external/logging.rkt")
+
+(provide (all-defined-out) (all-from-out "external/logging.rkt"))
+
+;; creates `pollen-logger` and associated functions:
+;; log-pollen-fatal, log-pollen-error, log-pollen-warning, 
+;; log-pollen-info, and log-pollen-debug
+(define-logger pollen) 
+
+(define (message . items)
+  (log-pollen-info (string-join (map ~a items) " ")))
