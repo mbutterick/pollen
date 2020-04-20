13e6524922aeb51073ac2de0f3773e53d4ad6fe6
Matthew Butterick <mb@mbtype.com>
2020-04-19 22:14:09 -0700

    notest

--- pollen/info.rkt
+++ pollen/info.rkt
@@ -4,6 +4,7 @@
 (define raco-commands '(("pollen" (submod pollen/private/command raco) "issue Pollen command" #f)))
 (define compile-omit-paths '("test" "tools" "server-extras" "scribblings/third-tutorial-files"))
 ;; it's redundant to test "pollen.scrbl" because it incorporates the other scribble sources by reference
-(define test-omit-paths '("test/data" "tools" "server-extras" "scribblings/third-tutorial-files" "scribblings/pollen.scrbl"))
+;; don't test app.rkt because it launches the gui
+(define test-omit-paths '("test/data" "tools" "server-extras" "scribblings/third-tutorial-files" "scribblings/pollen.scrbl" "app.rkt"))
 ;; don't put #"p" in this list because it's not a #lang
 (define module-suffixes '(#"pp" #"pm" #"pmd" #"ptree"))
