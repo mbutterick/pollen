ae29f16513acbdf17460b81b2a1db4cf9270e3d3
Matthew Butterick <mb@mbtype.com>
2020-05-19 14:15:48 -0700

    don't test doc sources

--- pollen/info.rkt
+++ pollen/info.rkt
@@ -4,6 +4,6 @@
 (define raco-commands '(("pollen" (submod pollen/private/command raco) "issue Pollen command" #f)))
 (define compile-omit-paths '("test" "tools" "server-extras" "scribblings/third-tutorial-files"))
 ;; it's redundant to test "pollen.scrbl" because it incorporates the other scribble sources by reference
-(define test-omit-paths '("test/data" "tools" "server-extras" "scribblings/third-tutorial-files" "scribblings/pollen.scrbl"))
+(define test-omit-paths '("test/data" "tools" "server-extras" "scribblings/third-tutorial-files" "scribblings"))
 ;; don't put #"p" in this list because it's not a #lang
 (define module-suffixes '(#"pp" #"pm" #"pmd" #"ptree"))
