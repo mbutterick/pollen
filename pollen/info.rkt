e73517315cf3536307552f6400a5abfb4c2b3322
Matthew Butterick <mbutterick@users.noreply.github.com>
2021-05-18 13:35:18 -0700

    suppress test-project-port.rkt
    Doesn’t work with GH actions consistently

--- pollen/info.rkt
+++ pollen/info.rkt
@@ -4,6 +4,6 @@
 (define raco-commands '(("pollen" (submod pollen/private/command raco) "issue Pollen command" #f)))
 (define compile-omit-paths '("test" "tools" "server-extras" "scribblings/third-tutorial-files"))
 ;; it's redundant to test "pollen.scrbl" because it incorporates the other scribble sources by reference
-(define test-omit-paths '("test/data" "tools" "server-extras" "scribblings/third-tutorial-files"))
+(define test-omit-paths '("test/data" "tools" "server-extras" "scribblings/third-tutorial-files" "test/test-project-port.rkt"))
 ;; don't put #"p" in this list because it's not a #lang
 (define module-suffixes '(#"pp" #"pm" #"pmd" #"ptree"))
